8866ea5dcea9a1665b9fbbc954d870e1c5b8b093
Adrián Naranjo <adriannaranjosanchez@gmail.com>
2024-09-30 11:10:24 +0200

    test endpoint added

--- node-app/app.js
+++ node-app/app.js
@@ -5,6 +5,9 @@ app.get("/api/test", (req, res) => {
   res.json({ message: "Hello from Express!" });
 });
 
+app.post("/api/test", (req, res) => {
+  res.json({ message: "Hello from Express with a POST Verb!" });
+});
 const PORT = 3000;
 app.listen(PORT, () => {
   console.log(`Express server running on port ${PORT}`);
